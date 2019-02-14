e16d478e57ebdbd77a5367e4b30520a5125e339e
larryback <41230266+larryback@users.noreply.github.com>
2019-02-15 01:43:31 +0900

    2월15일새벽집

---
+++ js/test.js
@@ -0,0 +1,16 @@
+var s = "javascript";
+console.log(s);
+var num = "100";
+console.log(num);
+var tag = "<h1>String</h1>";
+console.log(tag);
+var a = 100;
+var t = Number("500");  // "500" --> 500
+console.log(a, t);
+var b = true;
+var c = 10 >= 100;          //false
+var k = Boolean("Hello");  //true
+console.log(b, c, k);
+var l = Boolean(0);   //false
+var m = Boolean("홍길동")  //true
+console.log(l, m);
